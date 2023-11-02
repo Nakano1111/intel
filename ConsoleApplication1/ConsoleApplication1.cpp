8e0237754ade0b34a697f3c77ec2cc9992f770ae
민석 이 <dlalstjr8@naver.com>
2023-11-02 16:51:51 +0900

    20231102

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -13,7 +13,10 @@ public:
     {
         x = x1; y = y1;
     }
+    void setP(int x1 = 0, int y1 = 0) { x = x1; y = y1;}
+}
     double Dist(Point p);
+    double Dist(Point p, point p2);
 };  
 
 double point::Dist(Point p);
@@ -24,6 +27,7 @@ double point::Dist(Point p);
     return d;
 }
 
+
 int main()
 {
     printf("안녕하세요 C++ 의 세계에 오신걸 환영합니다!\n\n");
@@ -33,15 +37,26 @@ int main()
     printf("1.2의 제곱: %f\n", SQIARE(1.2));
     int x = 7;
     printf("%d의 제곱: %d\n", x + 1, SQIARE(x + 1));
-    printf("-5의 절대값:%d\n",abs(-5));
-    printf("7의 절대값:%d\n", SQIARE(abs(7)));
+    printf("-5의 절대값:%d\n", abs(-5));
+    printf("7의 절대값:%d\n", SQIARE(abs(7));
 
-    Point p1(10, 10), p2(20, 30);
+    double d = p1.Dist(p2);
+    double e = p1.Dist(20, 30);
+    double d = p1.Dist(p2);
+    double e = p1.Dist(p1, p2);
+    printf("두점 p1(%d,%d),p2(%d,%d)의 거리는 %.2ㄹ(%,2f), 면적은 %d 입니다.\r\n",
+        p1.x, p1.y, p2.x, p2.y, d, e, p2.Area(p1));
+    printf("두점 p1(%d,%d),p2(%d,%d)의 거리는 %.2ㄹ(%,2f), 면적은 %d 입니다.\r\n",
+        p1.x, p1.y, p2.x, p2.y, d, e, p2.Area(p1));
+    printf(""null ar)
+
+        Point p1(10, 10), p2(20, 30);
     p1.x = 10; p1.y = 10;
     p2.x = 20; p2.y = 30;
-    
+
     double d = p1, dist(p2);
-    printf("두점 p1(%d,%d),p2(%d,%d)의 거리는 %.%f 입니다", p1.x,p1.y,p2,x,p2,y)
+    printf("두점 p1(%d,%d),p2(%d,%d)의 거리는 %.%f 입니다", p1.x, p1.y, p2.x, p2.y)
+}
     
     //std::cout <<"안녕하세여. C++ 의 세계에 오신걸 환영합니다!\n"
 double dist(Point p1, printf p2)
@@ -53,4 +68,43 @@ double dist(Point p1, printf p2)
         double d = sqrt(w1 + h1);
         return d;
     }
+    void printp(point p)
+    void swap_v(int a, int b)
+    void swap_p(int *a, int *b)
+    void swap(int a, int b)   //Call-by-referenc
+{
+        int t(*a);
+        printf("[swap_r]초기값 a = %d b = %d\n", a, b);
+        *a = *b; *b = t;
+        printf("[swap_r]결과값 a = %d b = %d\n", a, b);
+}
+int main()
+{
+        int a(10), b(20); //a = 10; b = 20;
+        printf("변수 a = \b b = %d\n", a, b);
+        swap(a, b);
+        printf("swap 후 a = \b b = %d\n", a = 30, b = 43);
+}
+int main()
+{
+    Point arr[3];  //3개의 point 클래스 객체가 default 초기화 됨.
+    arr[0].setP(10, 10);
+    arr[1].setP(20, 30);
+    arr[2].setP(40, 60);
+
+    //printf("class point 객체 크기 : %d\n", sizeof(point));
+    Point* parr[3]; //point 객체포인터 (*) 배열
+    printf("point 객체 포인터 배열 크기 : %d\n", sizeof(parr));
+    parr[0].->serp(10, 10); printp(*parr[0]);
+    parr[0].->serp(20, 30); printp(*parr[1]);
+    parr[0].->serp(40, 50); printp(*parr[2]);
+
+
+    int n = 1234, n1 = 1000;
+    int& m = n;
+    printf("{레퍼런스 변수 테스트} n = %d, n = \d m = %d\n", n, m);
+    m = n1;
+    m = 5678;
+    printf("{레퍼런스 변수 테스트} m = %d, n = %d m = %d\n", n, m);
+}
 }
---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,79 @@
+﻿#include <iostream>
+#define MIN(x,y)  (((x>y))?(y):(x))
+#define MAX(x,y)  (((x<y))?(y):(x))
+class point
+{
+private:   // default 접근 지시자 (외부 참조 불가)
+
+public:    //   "   (외부 참조 가능)
+
+    int x, y;   // 맴버 변수 선언. 객채(나 자신)의 좌표
+    point(int x1 = 0, int y1 = 0) : x(x1 + 1), y(y1 + 1)  //initiallzer
+        //생성자 정의, 함수적 특성(defalt, overload 등)
+    {
+        //x = x1; y = y1;
+    }
+    point(point p)
+    {
+        int w = x - p.x;
+        int h = y - p.y;
+        return sqrt(w * w + h * h);
+    }
+};
+class Point(...);
+
+class rect
+{
+public:
+    point p11, p22; //z클래스 맴버변수 선언
+    point LL, LR, UL, UR;
+    rect(point p1, point p2)
+   {
+        int xl = MIN(p1.x, p2.x);
+        int xr = MAX(p1.x, p2.x);
+        int yl = MIN(p1.y, p2.y); 
+        int yu = MAX(p1.y, p2.y);
+        LL = Point(xl, yl);
+        LR = Point(xr, yl);
+        UL = Point(xl, yu);
+        UR = Point(xr, yu);
+        p11 = p1; p22 = p2;
+   }
+    point GetLL(xl, xr);
+    point GetLR(xl, yu);
+    point GetUL(yl, xr);
+    point GetUR(yl, yu);
+}
+
+
+void Printp(point p);
+{
+    printf("point(%d,%d)")
+}
+    double Dist(point p1, point p2)  // 맴버 함수 : 두 점(자신을 제외한)간의 거리 계산. doible 값으로 return
+    {
+        int w = p1.x - p2.x;
+        int h = p1.y = p2.y;
+        return sqet(w * w + h * h);
+    }
+};
+int main()
+{
+    point p1, p2(10, 10), p3(20, 30), p4;
+    p4 = p3 ;
+    double d = p1.Dist(p2);
+    double d1 = p2.Dist(p3);
+    double d2 = p1.Dist(p2, p3);
+    Printp(p1);   Printp(p2);   Printp(p3);
+    printf("점간의 거리 계산 : \nd = %.2f   d1 = %.2f   d2 = %.2f, d, d1 ,d2");
+
+    rect r(p2, p3);
+    printf("두점\n");
+    Printp(p2);
+    Printp(p3);
+    printf("에 의한 정의된 사각형의 꼭지점의 좌표는\n");
+    Printp(r, GetLL);
+    Printp(r, GetLR);
+    Printp(r, GetUL);
+    Printp(r, GetUR);
+}
